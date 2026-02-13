4137062865a976e5c28a4c5b52b3bc1b49367092
daniel-kovacic <kovacic-daniel@gmx.at>
2026-02-13 15:14:39 +0100

    Add model fixture

--- ml_training/tests/conftest.py
+++ ml_training/tests/conftest.py
@@ -3,6 +3,9 @@ from PIL import Image
 
 import pytest
 
+from config import LANDMARK_COORD_SHAPE
+from models.model import ResNetModel
+
 
 @pytest.fixture
 def sample_data_dir() -> Path:
@@ -15,3 +18,8 @@ def sample_image_path(tmp_path: Path):
     test_image = Image.new("RGB", (100, 200))
     test_image.save(image_path)
     return image_path
+
+
+@pytest.fixture
+def model():
+    return ResNetModel(LANDMARK_COORD_SHAPE)
